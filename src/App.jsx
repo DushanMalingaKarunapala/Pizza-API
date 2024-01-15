afdf5c8214c9d2cdbcca96cf7f36d09bd0354626
K Dushan Malinga Karunapala <dushanforrest343@gmail.com>
2024-01-15 17:54:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Pizza } from "./components/pizza";
+import { pizzaData } from "../public/data";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>aneeeee appa</h1>
+        <Pizza />
+        <Pizza />
+        <Pizza />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/pizza.jsx
@@ -0,0 +1,10 @@
+import { pizzaData } from "../../public/data";
+export const Pizza = () => {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h1>Foccasia</h1>
+      <h3>Bread with italian olive oil and rosemary</h3>
+    </div>
+  );
+};
